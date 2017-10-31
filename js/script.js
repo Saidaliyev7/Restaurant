8afb5b7eba9ac4de738a3de57420ee5075292f3f
Said <mrsaidaliyev@hotmail.com>
2017-10-31 16:35:44 +0400

    90% hazir azaldi :D

--- js/script.js
+++ js/script.js
@@ -5,4 +5,21 @@ $(document).ready(function () {
         return false;
     });
     new WOW().init();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        dots:true,
+        autoplay:true,
+        responsiveClass:true,
+       items:1
+    })
+
+    var x =
+
+$(window).scroll(function() {
+   $('body').animate({
+       scrollTop:$('.DeliciousFood').offset().top;
+   })
+    
+})
+
 });
